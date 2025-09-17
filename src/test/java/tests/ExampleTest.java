148bc90925b85fcce3534aa123e4ed1975bcccaf
Новолодский Алексей Юрьевич 0 <novolodskiyay@it.mos.ru>
2025-09-17 23:42:49 +0300

    Initial commit

---
+++ src/test/java/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+    @Test
+    void shouldBeEqualsTwo() {
+        assertEquals(2, 1 + 1);
+    }
+}
